4fe22d16fd883f901bda8a857086a19e038403ec
Shripal Mehta <73225806+shripalm@users.noreply.github.com>
2022-06-19 21:21:35 +0530

    Logs Handling

--- entry.php
+++ entry.php
@@ -9,23 +9,6 @@
     $matchedRouteData = routeMatcher($method);
     $fileName = __DIR__ . '/_view' . $matchedRouteData["path"];
     if (! file_exists($fileName)) retResponse(404, "Route Defined, File Not Found - $fileName");
-    /* 
-        This essentialCall invokes 
-            checkContentType --> Every time
-            getBearerToken  --> Only if auth is defined as true
-        
-        Hence, checkContentType invokes every time it should require some content all the time
-    */
     essentialCall($matchedRouteData["auth"]);
-
-    /*
-        Gets json from the body, means you must pass json every time
-        However, you can change it...
-
-        Valid -->
-            $data = $_POST
-            $data = $_FILES
-            $data = $_GET
-    */
     $data = getJsonFromBody();
     include_once($fileName);
--- index.php
+++ index.php
@@ -1,7 +1,26 @@
 <?php
 
     define('BASE_PHYSICAL_PATH', __DIR__);
+    define('TIME_ZONE', '+0530');
     function retResponse($code, $message, $data=[]){
+        
+        $date = new \DateTime(date('m/d/Y H:i:s', $_SERVER['REQUEST_TIME']));
+        $date->setTimezone(new \DateTimeZone(TIME_ZONE));
+        $logInfo = 
+        "\n-------------------------------------------------------".
+        "\nREMOTE_ADDR: ".$_SERVER['REMOTE_ADDR'].
+        "\nREMOTE_PORT: ".$_SERVER['REMOTE_PORT'].
+        "\nREQUEST_URI: ".$_SERVER['REQUEST_URI'].
+        "\nREQUEST_METHOD: ".$_SERVER['REQUEST_METHOD'].
+        "\nREQUEST_TIME: ".$_SERVER['REQUEST_TIME']." | ".$date->format('Y-m-d H:i:s').
+        "\nRESPONSE_CODE: $code".
+        "\nRESPONSE_MSG: $message".
+        "\n-------------------------------------------------------";
+        $fname = BASE_PHYSICAL_PATH.'/_logs/req-res/'.$date->format('Y-m-d').'.txt';
+        $fp = fopen($fname, 'a');
+        fwrite($fp, $logInfo);
+        fclose($fp);
+        
         http_response_code($code);
         header('Content-Type: application/json; charset=utf-8');
         echo json_encode(["msg"=>$message,"data"=>$data]);
@@ -10,6 +29,21 @@
     try {
         include_once(BASE_PHYSICAL_PATH.'/entry.php');
     } catch (\Throwable $th) {
+        
+        $date = new \DateTime(date('m/d/Y H:i:s', $_SERVER['REQUEST_TIME']));
+        $date->setTimezone(new \DateTimeZone(TIME_ZONE));
+        $logInfo = 
+        "\n-------------------------------------------------------".
+        "\nMESG: ".$th->getMessage().
+        "\nFILE: ".$th->getFile().
+        "\nLINE: ".$th->getLine().
+        "\nCODE: ".$th->getCode().
+        "\n-------------------------------------------------------";
+        $fname = BASE_PHYSICAL_PATH.'/_logs/error/'.$date->format('Y-m-d').'.txt';
+        $fp = fopen($fname, 'a');
+        fwrite($fp, $logInfo);
+        fclose($fp);
+        
         retResponse(500, "Internal Server Error", [
             "msg"  => $th->getMessage(),
             "file" => $th->getFile(),
